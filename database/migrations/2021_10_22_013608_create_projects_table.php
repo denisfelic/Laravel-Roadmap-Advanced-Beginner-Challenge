a2c12474fe00dda4d42428ac12801e808914422d
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-22 13:10:59 -0300

    add entites and migrations

---
+++ database/migrations/2021_10_22_013608_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedBigInteger('associated_user_id');
+            $table->unsignedBigInteger('associated_client_id');
+            $table->boolean('is_active');
+            $table->timestamp('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
