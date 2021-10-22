1e7f668bdea584ef65f9642c83adb6bf20056e9c
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-22 16:31:44 -0300

    add dashboard, auth, users and clients routes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('/')->middleware(['auth'])->group(function () {
+
+    Route::get('/', function () {
+        return  view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('/client', ClientController::class);
+    Route::resource('/users', UserController::class);
 });
+
+
+
+require __DIR__ . '/auth.php';
