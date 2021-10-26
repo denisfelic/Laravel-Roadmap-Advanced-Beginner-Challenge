7d8c69642b547d93adf15ce9ad5091245d38d2a5
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-26 14:57:45 -0300

    create project eloquent model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        "title",
+        "description",
+        "assigned_user_id",
+        "assigned_client_id",
+        "creator_user_id",
+        "deadline",
+        "status",
+    ];
+
+
+    /**
+     * Get the creatorUser that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creatorUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'foreign_key', 'other_key');
+    }
 }
