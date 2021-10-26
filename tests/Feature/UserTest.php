693ac87ee2a562377a1a902a9019617499f8c3b0
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-26 01:23:56 -0300

    add User tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,11 +8,11 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertTrue;
 
 class UserTest extends TestCase
 {
 
-    // use RefreshDatabase;
 
     /**
      * @test
@@ -169,8 +169,7 @@ class UserTest extends TestCase
         $anotherUser->save();
 
         $request = $this->actingAs($userAdmin)->withSession(['banned' => false])
-            ->post(route('users.change_role'), [
-                "id" => $anotherUser->id,
+            ->post(route('users.change_role', $anotherUser->id), [
                 "role" => "admin",
 
             ])
@@ -188,14 +187,14 @@ class UserTest extends TestCase
     {
         $normalUser = User::factory()->create();
         $normalUser->role = '';
+        $normalUser->save();
         // certify that user is an normal user
         $anotherUser = User::factory()->create();
         $anotherUser->role = '';
         $anotherUser->save();
 
         $request = $this->actingAs($normalUser)->withSession(['banned' => false])
-            ->post(route('users.change_role'), [
-                "id" => $anotherUser->id,
+            ->post(route('users.change_role', $anotherUser->id), [
                 "role" => "admin",
 
             ])
@@ -203,6 +202,27 @@ class UserTest extends TestCase
 
         $anotherUser = User::find($anotherUser->id);
         assertEquals('', $anotherUser->role);
+    }
+
+    public function test_check_if_a_normal_user_cannot_change_role_of_admins()
+    {
+        $normalUser = User::factory()->create();
+        $normalUser->role = '';
+        $normalUser->save();
+        // certify that user is an normal user
+        $adminUser = User::factory()->create();
+        $adminUser->role = 'admin';
+        $adminUser->save();
+
+        $request = $this->actingAs($normalUser)->withSession(['banned' => false])
+            ->post(route('users.change_role', $adminUser->id), [
+                "role" => "admin",
+
+            ])
+            ->assertStatus(403);
+
+        $adminUser = User::find($adminUser->id);
+        assertEquals('admin', $adminUser->role);
     }
 
     /**
@@ -217,8 +237,7 @@ class UserTest extends TestCase
         $anotherUser->role = '';
         $anotherUser->save();
 
-        $request = $this->post(route('users.change_role'), [
-            "id" => $anotherUser->id,
+        $request = $this->post(route('users.change_role', $anotherUser->id), [
             "role" => "admin",
 
         ])
@@ -237,7 +256,113 @@ class UserTest extends TestCase
      */
     public function test_check_if_an_admin_cannot_change_role_of_another_user_admin()
     {
-        //TODO
-        assertEquals(true, false);
+        $userAdmin1 = User::factory()->create();
+        $userAdmin2 = User::factory()->create();
+        $userAdmin1->role = 'admin';
+        $userAdmin2->role = 'admin';
+
+        $userAdmin1->save();
+        $userAdmin2->save();
+
+        $this->actingAs($userAdmin1)->post(route('users.change_role', $userAdmin2->id), ["role" => ""])
+            ->assertForbidden();
+
+        $userAdmin2->refresh();
+        assertEquals('admin', $userAdmin2->role);
+    }
+
+    public function test_check_if_admin_can_delete_normal_user()
+    {
+        $admin1 = User::factory()->create();
+        $normalUser = User::factory()->create();
+
+        $admin1->role = 'admin';
+        $normalUser->role = '';
+
+        $admin1->save();
+        $normalUser->save();
+
+        $this->actingAs($admin1)->delete(route('users.destroy', $normalUser->id))
+            ->assertOk()
+            ->assertJson($normalUser->toArray());
+
+        $checkUser = User::find($normalUser->id);
+        $this->assertNull($checkUser);
+    }
+
+    public function test_check_if_admin_cannot_delete_another_admin()
+    {
+        $admin1 = User::factory()->create();
+        $admin2 = User::factory()->create();
+
+        $admin1->role = 'admin';
+        $admin2->role = 'admin';
+
+        $admin1->save();
+        $admin2->save();
+
+        $this->actingAs($admin1)->delete(route('users.destroy', $admin2->id))
+            ->assertForbidden();
+
+        $checkUser = User::find($admin2->id);
+        $this->assertNotNull($checkUser);
+        $this->assertEquals($admin2->toArray(), $checkUser->toArray());
+    }
+
+    public function test_check_if_normal_user_cannot_delete_normal_user()
+    {
+        $normalUser = User::factory()->create();
+        $anotherUser = User::factory()->create();
+
+        $normalUser->role = '';
+        $normalUser->save();
+
+
+        $anotherUser->role = '';
+        $anotherUser->save();
+        $adminData = $anotherUser->toArray();
+
+        $this->actingAs($normalUser)->withSession(['banned' => false])->delete(route('users.destroy', $anotherUser->id))
+            ->assertForbidden();
+
+        $expectedAnotherUser = User::find($anotherUser->id);
+        $this->assertEquals($adminData, $expectedAnotherUser->toArray());
+        $this->assertNotNull($expectedAnotherUser);
+    }
+
+    public function test_check_if_normal_user_cannot_delete_admin()
+    {
+        $normalUser = User::factory()->create();
+        $admin1 = User::factory()->create();
+
+        $normalUser->role = '';
+        $normalUser->save();
+
+
+        $admin1->role = 'admin';
+        $admin1->save();
+        $adminData = $admin1->toArray();
+
+        $this->actingAs($normalUser)->delete(route('users.destroy', $admin1->id))
+            ->assertForbidden();
+
+        $expectedAdminUser = User::find($admin1->id);
+        $this->assertEquals($adminData, $expectedAdminUser->toArray());
+        $this->assertNotNull($expectedAdminUser);
+    }
+
+    public function test_check_if_unauthenticated_user_cannot_delete_anybody()
+    {
+        $user = User::factory()->create();
+        $user->role = "";
+        $user->save();
+        $userData = $user->toArray();
+
+        $this->delete(route('users.destroy', $user->id))
+            ->assertRedirect();
+
+        $expectedUser = User::find($user->id);
+        $this->assertNotNull($expectedUser->toArray());
+        $this->assertEquals($userData, $expectedUser->toArray());
     }
 }
