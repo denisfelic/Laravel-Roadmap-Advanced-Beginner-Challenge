96a9308a14320556ab9e51d8f7135bcfb57fab09
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-29 19:21:53 -0300

    add test cases for ProjectService

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Client;
 use App\Models\Project;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,38 +16,137 @@ class ProjectTest extends TestCase
     public function test_admins_can_create_a_project()
     {
         $admin = User::factory()->create();
-        $project = Project::factory()->make();
+        $admin->role = 'admin';
+        $admin->save();
+        $client = Client::factory()->create();
+        $assignedUser = User::factory()->create();
+        $projectData = [
+            'title' => 'title test',
+            'description' => 'description test',
+            'status' => 'open',
+            'assigned_client_id' => $client->id,
+            'assigned_user_id' => $assignedUser->id,
+            'deadline' => Carbon::now()->addDays(3)->toString(),
 
+        ];
 
-        $this->actingAs($admin)->post(route('project.store'), $project->toArray())
+        $this->actingAs($admin)->post(route('project.store'), $projectData)
             ->assertOk()
-            ->assertJson($project->toArray());
+            ->assertJsonFragment($projectData);
 
-        $this->assertEquals($project->toArray(), Project::first()->toArray());
+        $this->assertEquals($client->id, Project::first()->client->id);
     }
 
     public function test_normal_user_cannot_create_a_project()
     {
+        $normalUser = User::factory()->create();
+        $normalUser->role = '';
+        $normalUser->save();
+        $client = Client::factory()->create();
+        $assignedUser = User::factory()->create();
+        $projectData = [
+            'title' => 'title test',
+            'description' => 'description test',
+            'status' => 'open',
+            'assigned_client_id' => $client->id,
+            'assigned_user_id' => $assignedUser->id,
+            'deadline' => Carbon::now()->addDays(3)->toString(),
+
+        ];
+
+        $this->actingAs($normalUser)->post(route('project.store'), $projectData)
+            ->assertForbidden();
+
+        $this->assertNull(Project::first());
     }
 
     public function test_unauthenticated_user_cannot_create_a_project()
     {
+        $client = Client::factory()->create();
+        $assignedUser = User::factory()->create();
+        $projectData = [
+            'title' => 'title test',
+            'description' => 'description test',
+            'status' => 'open',
+            'assigned_client_id' => $client->id,
+            'assigned_user_id' => $assignedUser->id,
+            'deadline' => Carbon::now()->addDays(3)->toString(),
+
+        ];
+
+        $this->post(route('project.store'), $projectData)
+            ->assertRedirect(route('login'));
+
+        $this->assertNull(Project::first());
     }
 
     public function test_admins_can_update_a_project()
     {
+
+        $project = Project::factory()->create();
+        $admin = $project->creator;
+
+        $updateData =  $project->toArray();
+        unset($updateData["creator"], $updateData["id"]);
+        $updateData["title"] = "Título alterado 123";
+
+        $this->actingAs($admin)->patch(route('project.update', $project->id), $updateData)
+            ->assertOk()
+            ->assertJson($updateData);
+
+        $expectedProject = Project::first();
+
+        $this->assertEquals($updateData["title"], $expectedProject->title);
     }
 
     public function test_normal_user_cannot_update_a_project()
     {
+        $project = Project::factory()->create();
+        $normalUser = User::factory()->create();
+
+        $updateData =  $project->toArray();
+        unset($updateData["creator"], $updateData["id"]);
+        $updateData["title"] = "Título alterado 123";
+
+        $this->actingAs($normalUser)->patch(route('project.update', $project->id), $updateData)
+            ->assertForbidden();
+
+        $expectedProject = Project::first();
+
+        $this->assertEquals($project->toArray(), $expectedProject->toArray());
     }
 
     public function test_unauthenticated_user_cannot_update_a_project()
     {
+        $project = Project::factory()->create();
+        $normalUser = User::factory()->create();
+
+        $updateData =  $project->toArray();
+        unset($updateData["creator"], $updateData["id"]);
+        $updateData["title"] = "Título alterado 123";
+
+        $this->patch(route('project.update', $project->id), $updateData)
+            ->assertRedirect(route('login'));
+
+        $expectedProject = Project::first();
+
+        $this->assertEquals($project->toArray(), $expectedProject->toArray());
     }
 
     public function test_admins_can_delete_a_project()
     {
+        $project = Project::factory()->create();
+        $admin = $project->creator;
+
+        $projectData = $project->toArray();
+
+        $this->actingAs($admin)->delete(route('project.destroy', $project->id))
+            ->assertOk()
+            ->assertJson($projectData);
+
+        $expectedProject = Project::first();
+
+        $this->assertNull($expectedProject);
     }
 
     public function test_normal_user_cannot_delete_a_project()
